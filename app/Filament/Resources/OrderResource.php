9e5ce42957ea1f6cc5aae38dd5f818a40c1c2c7d
rizhul.januar <rizhul.januar@kopnus.com>
2025-05-24 21:35:45 +0700

    feat: create crud orders

---
+++ app/Filament/Resources/OrderResource.php
@@ -0,0 +1,213 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\OrderResource\Pages;
+use App\Filament\Resources\OrderResource\RelationManagers;
+use App\Models\Order;
+use App\Models\Product;
+use Filament\Forms;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Form;
+use Filament\Notifications\Notification;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class OrderResource extends Resource
+{
+    protected static ?string $model = Order::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?int $navigationSort = 2;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Group::make()
+                    ->schema([
+                        Forms\Components\Section::make('Info Utama')
+                            ->schema([
+                                Forms\Components\TextInput::make('name')
+                                    ->required()
+                                    ->maxLength(255),
+                                Forms\Components\Select::make('gender')
+                                    ->options([
+                                        'male' => 'Laki-laki',
+                                        'female' => 'Perempuan'
+                                    ])
+                                    ->required(),
+                            ])
+                    ]),
+
+                Forms\Components\Group::make()
+                    ->schema([
+                        Forms\Components\Section::make('Info Tambahan')
+                            ->schema([
+                                Forms\Components\TextInput::make('email')
+                                    ->email()
+                                    ->maxLength(255),
+
+                                Forms\Components\TextInput::make('email')
+                                    ->email()
+                                    ->maxLength(255),
+
+                                Forms\Components\DatePicker::make('birthday'),
+                            ])
+                    ]),
+
+                    Forms\Components\Section::make('Produk dipesan')->schema([
+                        self::getItemsRepeater(),
+                    ]),
+
+
+                    Forms\Components\Group::make()
+                        ->schema([
+                            Forms\Components\Section::make()
+                                ->schema([
+                                    Forms\Components\TextInput::make('total_price')
+                                        ->required()
+                                        ->readOnly()
+                                        ->numeric(),
+
+                                    Forms\Components\Textarea::make('note')
+                                        ->columnSpanFull(),
+                                ])
+                        ]),
+
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListOrders::route('/'),
+            'create' => Pages\CreateOrder::route('/create'),
+            'edit' => Pages\EditOrder::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getItemsRepeater(): Repeater
+    {
+        return Repeater::make('orderProducts')
+            ->relationship()
+            ->live()
+            ->columns([
+                'md' => 10,
+            ])
+            ->afterStateUpdated(function (Forms\Get $get, Forms\Set $set): void {
+                self::updateTotalPrice($get, $set);
+            })
+            ->schema([
+                Forms\Components\Select::make('prodcut_id')
+                    ->label('Produk')
+                    ->required()
+                    ->options(Product::query()->where('stock', '>', 1)->pluck('name', 'id'))
+                    ->columnSpan([
+                        'md' => 5
+                    ])
+                    ->afterStateHydrated(function (Forms\Set $set, Forms\Get $get, $state) {
+                        $product = Product::find($state);
+                        $set('unit_price', $product->price ?? 0);
+                        $set('stock', $product->stock ?? 0);
+                    })
+                    ->afterStateUpdated(function ($state, Forms\Set $set, Forms\Get $get) {
+                        $product = Product::find($state);
+                        $set('unit_price', $product->price ?? 0);
+                        $set('stock', $product->stock ?? 0);
+                        $quantity = $get('quantity') ?? 1;
+                        $stock = $get('stock');
+                        self::updateTotalPrice($get, $set);
+                    })
+                    ->disableOptionsWhenSelectedInSiblingRepeaterItems(),
+                Forms\Components\TextInput::make('quantity')
+                    ->required()
+                    ->numeric()
+                    ->default(1)
+                    ->minValue(1)
+                    ->columnSpan([
+                        'md' => 1
+                    ])
+                    ->afterStateUpdated(function ($state, Forms\Set $set, Forms\Get $get) {
+                        $stock = $get('stock');
+                        if ($state > $stock) {
+                            $set('quantity', $stock);
+                            Notification::make()
+                                ->title('Stok tidak mencukupi')
+                                ->warning()
+                                ->send();
+                        }
+
+                        self::updateTotalPrice($get, $set);
+                    }),
+
+                Forms\Components\TextInput::make('stock')
+                    ->required()
+                    ->numeric()
+                    ->readOnly()
+                    ->columnSpan([
+                        'md' => 1
+                    ]),
+
+                Forms\Components\TextInput::make('unit_price')
+                    ->label('Harga saat ini')
+                    ->required()
+                    ->numeric()
+                    ->readOnly()
+                    ->columnSpan([
+                        'md' => 3
+                    ]),
+            ]);
+    }
+
+    protected static function updateTotalPrice(Forms\Get $get, Forms\Set $set)
+    {
+        $selectedProducts = collect($get('orderProducts'))->filter(fn ($item) => !empty($item['product_id']) && !empty($item['quantity']));
+
+        $prices = Product::find($selectedProducts->pluck('product_id'))->pluck('price', 'id');
+        $total = $selectedProducts->reduce(function ($total, $product) use ($prices) {
+            return $total + ($prices[$product['product_id']] * $product['quantity']);
+        }, 0);
+
+        $set('total_price', $total);
+    }
+
+    protected static function updateExcangePaid(Forms\Get $get, Forms\Set $set)
+    {
+        $paidAmount = (int) $get('paid_amount') ?? 0;
+        $totalPrice = (int) $get('total_price') ?? 0;
+        $exchangePaid = $paidAmount - $totalPrice;
+        $set('change_amount', $exchangePaid);
+    }
+}
